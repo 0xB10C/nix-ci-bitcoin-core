e12d5dd48f642f2bd91c3c9dbe0a00d5febad709
0xb10c <b10c@b10c.me>
2024-11-11 10:00:03 +0100

    extract cirrus-runner from runner.nix
    In the future, this can be come it's own module.

--- runner.nix
+++ runner.nix
@@ -54,60 +54,6 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH988C5DbEPHfoCphoW23MWq9M6fmA4UTXREiZU0J7n0 will.hetzner@temp.com"
     ];
 
-  # The cirrus worker requires a token to connect.
-  # Currently this requires manual positioning in /etc/cirrus/worker.env in the form:
-  # CIRRUS_TOKEN=<token>
-  systemd.services.cirrus-worker = {
-    description = "Cirrus CI Worker";
-    after = [ "network.target" "docker.service" ];
-    wants = [ "docker.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.cirrus-cli}/bin/cirrus worker run --token $CIRRUS_TOKEN --labels type=ax52_x86-64";
-      Restart = "always";
-      User = "cirrus-worker";
-      EnvironmentFile = "/etc/cirrus/worker.env";
-    };
-    environment = {
-      XDG_CACHE_HOME = "/var/lib/cirrus-worker/.cache";
-      PATH = lib.mkForce (lib.makeBinPath [
-        pkgs.bash
-        pkgs.coreutils
-        pkgs.findutils
-        pkgs.gnugrep
-        pkgs.gnused
-        pkgs.systemd
-        pkgs.cirrus-cli
-        pkgs.docker
-        pkgs.python3
-      ]);
-      DOCKER_HOST = "unix:///var/run/docker.sock";
-      RESTART_CI_DOCKER_BEFORE_RUN = "1";
-    };
-  };
-
-  users.users.cirrus-worker = {
-    isSystemUser = true;
-    group = "cirrus-worker";
-    description = "Cirrus CI worker user";
-    home = "/var/lib/cirrus-worker";
-    createHome = true;
-    shell = pkgs.bash;
-    extraGroups = [ "docker" ];
-  };
-  users.groups.cirrus-worker = {};
-
-  # Create /etc/cirrus directory and /var/lib/cirrus-worker/.cache
-  system.activationScripts = {
-    cirrusWorkerDir = ''
-      mkdir -p /etc/cirrus
-      chmod 755 /etc/cirrus
-      mkdir -p /var/lib/cirrus-worker/.cache
-      chown cirrus-worker:cirrus-worker /var/lib/cirrus-worker/.cache
-      chmod 700 /var/lib/cirrus-worker/.cache
-    '';
-  };
-
   system.stateVersion = "24.05";
 
   nix.settings = {
